ad23a3a65a310ecb2e7df2081f4b17f6542f5e3f
GoodbyeNJN <cc@fuckwall.cc>
2020-02-25 21:29:38 +0800

    update default config file

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,16 @@
 'use strict';
 
 /**
- * egg-nos default config
+ * egg-nos-plugin default config
  * @member Config#nos
- * @property {String} SOME_KEY - some description
+ * @property {String} accessKey - access key
+ * @property {String} accessSecret - access secret
+ * @property {String} endpoint - endpoint，不同地域不同
+ * @property {String} defaultBucket - 默认的 Bucket，如果不设置，那么需要在单独的每次请求中进行设置
  */
 exports.nos = {
-
+  accessKey: 'your-access-key',
+  accessSecret: 'your-access-secret',
+  endpoint: 'http://nos-eastchina1.126.net',
+  defaultBucket: 'nos-test',
 };
